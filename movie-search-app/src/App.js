738e2dfbe355ef67d4beea43edde27b027c37e62
stetson Harding <stetonharding@outlook.com>
2021-03-04 16:59:18 -0800

    Component setup and API fetch.

---
+++ movie-search-app/src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Movies from "./Movies"
+
+const App = () =>{
+
+   
+
+    return(
+     <Movies  />
+    )
+}
+
+export default App
---
+++ movie-search-app/src/Movie.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+import './movie.css'
+
+const Movie = ({movie}) =>{
+    return(
+        <div className="container card">
+            <img className="movie-poster" src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt="" />
+            <p className="movie-title">{movie.original_title}</p>
+            <p className="movie-description">{movie.overview}</p>
+            <p className="movie-releaseDate">{movie.release_date}</p>
+        </div>
+    )
+}
+
+export default Movie
