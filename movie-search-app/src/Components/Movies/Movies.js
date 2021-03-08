91e6ef1582147cc2daa08c230241adddc118eaec
Stetson Harding <stetson2001@msn.com>
2021-03-07 17:59:10 -0800

    pagination stage 1

--- movie-search-app/src/Components/Movies/Movies.js
+++ movie-search-app/src/Components/Movies/Movies.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
 import Movie from "../../Components/Movie/Movie";
+import Pagination from "./Pagination"
 
 import './Movies.css'
 
@@ -7,6 +8,8 @@ const Movies = () => {
     const [query, setQuery] = useState("")
     const [movies, setMovies] = useState([])
     const [loading, setLoading] = useState(true)
+    const [currentPage, setCurrentPage] = useState(1)
+    const [moviesPerPage] = useState(4)
 
 
     //fetch movies from url
@@ -20,6 +23,13 @@ const Movies = () => {
         setLoading(false)
     }
 
+    const indexOfLastMovie = currentPage * moviesPerPage
+    const indexOfFirstMovie = indexOfLastMovie - moviesPerPage
+    const currentMovie = movies.slice(indexOfFirstMovie, indexOfLastMovie)
+
+
+    //change pages
+    const paginate = (pageNumber) => setCurrentPage(pageNumber)
 
     return (
         <>
@@ -41,9 +51,11 @@ const Movies = () => {
             </form>
 
             <div className="Movie_list">
-                {movies.filter(movie =>movie.poster_path).map(movie =>
-                   loading ? <h2>Loading...</h2> : <Movie key={movie.id} movie={movie} />
+                {currentMovie.filter(movie =>movie.poster_path).map(movie =>
+                   <Movie key={movie.id} movie={movie} />
+                  
                 )}
+                <Pagination moviesPerPage={moviesPerPage} totalMovies={movies.length} paginate={paginate}/>
             </div>
         </>
     )
---
+++ movie-search-app/src/Components/Movies/Pagination.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export const Pagination = ({moviesPerPage, totalMovies, paginate}) => {
+
+    const pageNumbers = []
+
+    for(let i = 0; i <= Math.ceil(totalMovies / moviesPerPage); i++){
+        pageNumbers.push(i)
+    }
+    return (
+        <nav>
+            <ul className="pagination">
+                {pageNumbers.map(number =>(
+                    <li key={number} className="page-item">
+                        <a onClick={() => paginate(number)} href="!#" className="page-link">
+                            {number}
+                        </a>
+
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    )
+}
+
+
+export default Pagination
