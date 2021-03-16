5b6fc1a014df9d20afdd2f11fc141d4373357d81
Stetson Harding <stetson2001@msn.com>
2021-03-15 21:16:22 -0700

    added try/catch block

--- movie-search-app/src/Components/Movies/Movies.js
+++ movie-search-app/src/Components/Movies/Movies.js
@@ -10,31 +10,38 @@ const Movies = () => {
     const [loading, setLoading] = useState(false)
     const [currentPage, setCurrentPage] = useState(1)
     const [moviesPerPage] = useState(4)
+    const [handleError, setHandleError] = useState("")
 
-
-    //api
+    // api
     const url = `https://api.themoviedb.org/3/search/movie?api_key=a91d2a2db7fbe7eade901856811fccda&language=en-US&query=${query}&page=1&include_adult=false`;
 
     const searchMovies = async (e) => {
         e.preventDefault()
+
         //handle loading
         setLoading(true)
-       setTimeout(() => {
-        setLoading(false)
-       }, 3000);
-       //fetch movies api
-     
-        const moviePromise = await fetch(url)
-        const data = await moviePromise.json();
-        setMovies(data.results)
-    
-        
+        setTimeout(() => {
+            setLoading(false)
+        }, 3000);
+       
+        try {
+             //fetch movies
+            const moviePromise = await fetch(url)
+            const data = await moviePromise.json();
+            setMovies(data.results)
+
+           
+        }
+        catch (error) {
+            setHandleError("something went wrong")
+        }
     }
 
-
+    //getting current movie for pagination
     const indexOfLastMovie = currentPage * moviesPerPage
     const indexOfFirstMovie = indexOfLastMovie - moviesPerPage
     const currentMovie = movies.slice(indexOfFirstMovie, indexOfLastMovie)
+    // console.log(currentMovie)
 
     //change pages
     const paginate = (pageNumber) => setCurrentPage(pageNumber)
@@ -42,10 +49,10 @@ const Movies = () => {
     return (
         <>
             <h2 className="App-title">Have a movie in mind? Search for details!</h2>
-            {/* submit form */}
+            {/* form */}
             <form className="form" onSubmit={searchMovies}>
                 <label>
-                    Search Movies: 
+                    Search Movies:
                     <input
                         className="search-input"
                         type="text"
@@ -59,19 +66,22 @@ const Movies = () => {
             </form>
 
             <div className="Movie_list">
-                {/* Loading Logic */}
-                {loading ? 
-                <div className="loading-container">
-                    <i className="fas fa-video fa-3x loading-image"></i>
-                    <h1>loading...</h1>
-                </div> : 
-                currentMovie.filter(movie =>movie.poster_path).map(movie =>
-                   <Movie key={movie.id} movie={movie} />
-                )}
+                  {/* Loading Logic  */}
+                {loading ?
+                    <div className="loading-container">
+                        <i className="fas fa-video fa-3x loading-image"></i>
+                        <h1>loading...</h1>
+                    </div> :
+                    // display movies
+                    currentMovie.filter(movie => movie.poster_path).map(movie =>
+                        <Movie key={movie.id} movie={movie} />
+                    )}
             </div>
-                <div>
-                    {loading ? null :
-            <Pagination moviesPerPage={moviesPerPage} totalMovies={movies.length} paginate={paginate}/>}
+
+            <div><h1>{handleError}</h1></div>
+
+            <div className="paginations">
+                { loading ? null :<Pagination moviesPerPage={moviesPerPage} totalMovies={movies.length} paginate={paginate} /> }
             </div>
         </>
     )
